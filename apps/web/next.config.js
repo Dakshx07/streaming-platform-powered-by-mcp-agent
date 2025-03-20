96144f3aff71e3feaeaac978fcbe711180250b41
Daksh Hiran <daksh.hiran@gmail.com>
2025-03-20 23:43:03 +0530

    Add Next.js configuration

---
+++ apps/web/next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost', 'streamhub.s3.amazonaws.com'],
+  },
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    });
+    return config;
+  },
+}
